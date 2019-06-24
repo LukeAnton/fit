01d49682f5213297a6e7b979f1d4cebac43ffab9
Luke Firth <lukeantonfirth@gmail.com>
2019-06-24 11:56:00 +1000

    stage

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use("/api/trainerprofile", require("./routes/api/trainerprofile"));
 //Serve static assets in prod
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
